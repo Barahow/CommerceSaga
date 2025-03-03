639e49c79b5ed03dd772b7ef2dcb680c9cd97b91
Barahow <barahow1994@gmail.com>
2025-03-03 16:05:20 +0000

    adding core application with needed depencency

---
+++ commerce-saga/src/test/java/dev/barahow/commerce_saga/CommerceSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.barahow.commerce_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
